21263ddefeea3d29c3b3b58f562460849df008e5
Techuvo <techuvo.dev@gmail.com>
2026-08-26 08:22:39 -0400

    Fix GitHub Pages routing and mobile menu

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,54 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter,
-  Routes,
+  HashRouter,
   Route,
+  Routes,
 } from 'react-router-dom';
-
-import AppShell from './components/AppShell';
-
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Collections from './pages/Collections';
-import Story from './pages/Story';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
 
 import './index.css';
 
-function App() {
+import AppShell from './components/AppShell';
+
+const Home = lazy(() => import('./pages/Home'));
+const Shop = lazy(() => import('./pages/Shop'));
+const Collections = lazy(() => import('./pages/Collections'));
+const Story = lazy(() => import('./pages/Story'));
+const Product = lazy(() => import('./pages/Product'));
+const Cart = lazy(() => import('./pages/Cart'));
+
+function Loader() {
   return (
-    <BrowserRouter>
-      <AppShell>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/collections" element={<Collections />} />
-          <Route path="/story" element={<Story />} />
-          <Route path="/product/:slug" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </AppShell>
-    </BrowserRouter>
+    <div
+      style={{
+        minHeight: '70vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '10px',
+        letterSpacing: '0.14em',
+      }}
+    >
+      LOADING
+    </div>
   );
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <AppShell>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/collections" element={<Collections />} />
+            <Route path="/story" element={<Story />} />
+            <Route path="/product/:slug" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Suspense>
+      </AppShell>
+    </HashRouter>
   </React.StrictMode>
 );
